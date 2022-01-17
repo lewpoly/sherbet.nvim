bf95ef6fc10c740605021d586dfb860414bf6ce2
lewpoly <ldixon.design@gmail.com>
2022-01-16 20:26:53 -0500

    highlights and cmp bg

--- lua/darkplus/Packer.lua
+++ lua/darkplus/Packer.lua
@@ -2,7 +2,7 @@ local Packer = {
 		packerString = {fg = C.ui_orange, },
 		packerHash = {fg = C.ui4_blue, },
 		packerOutput = {fg = C.ui_purple, },
-		packerRelDate = {fg = C.alt_bg, },
+		packerRelDate = {fg = C.cyan, },
 		packerSuccess = {fg = C.success_green, },
 		packerStatusSuccess = {fg = C.ui4_blue, },
 }
