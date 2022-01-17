18310bb1911e2b7637efb9026bfe92546ce072f0
lewpoly <ldixon.design@gmail.com>
2022-01-16 20:39:36 -0500

    daisy

--- lua/darkplus/Dashboard.lua
+++ lua/darkplus/Dashboard.lua
@@ -1,6 +1,6 @@
 local Dashboard = {
 		DashboardHeader = {fg = C.yellow, },
-		DashboardCenter = {fg = C.light_blue, },
+		DashboardCenter = {fg = C.light_red, },
 		DashboardFooter = {fg = C.cyan, },
 }
 
