57b408843afa03395763387cbbf36a936cc16ece
lewpoly <ldixon.design@gmail.com>
2022-01-30 13:05:09 -0500

    changed to more complimentary

--- lua/gruvplus/StatusLine.lua
+++ lua/gruvplus/StatusLine.lua
@@ -1,6 +1,6 @@
 local StatusLine = {
-		StatusLine = {fg = "#090c10", bg = C.gray, },
-		StatusLineNC = {fg = "#090c10", bg = C.gray, },
+		StatusLine = {fg = "#1b1f24", bg = C.gray, },
+		StatusLineNC = {fg = "#1b1f24", bg = C.gray, },
 		StatusLineSeparator = {fg = C.bg, },
 		StatusLineTerm = {fg = C.alt_bg, },
 		StatusLineTermNC = {fg = C.alt_bg, },
