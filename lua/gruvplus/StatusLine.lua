aee5381d4d76a70010b04fa62d1c3193cd3f203a
lewpoly <ldixon.design@gmail.com>
2022-01-30 13:14:10 -0500

    went a little darker

--- lua/gruvplus/StatusLine.lua
+++ lua/gruvplus/StatusLine.lua
@@ -1,6 +1,6 @@
 local StatusLine = {
-		StatusLine = {fg = "#1b1f24", bg = C.gray, },
-		StatusLineNC = {fg = "#1b1f24", bg = C.gray, },
+		StatusLine = {fg = "#111317", bg = C.gray, },
+		StatusLineNC = {fg = "#111317", bg = C.gray, },
 		StatusLineSeparator = {fg = C.bg, },
 		StatusLineTerm = {fg = C.alt_bg, },
 		StatusLineTermNC = {fg = C.alt_bg, },
