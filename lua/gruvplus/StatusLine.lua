1e695501e6f307f8c0fe28a8ad781cd9d433027d
lewpoly <ldixon.design@gmail.com>
2022-02-19 12:53:35 -0500

    matched statusline

--- lua/gruvplus/StatusLine.lua
+++ lua/gruvplus/StatusLine.lua
@@ -1,6 +1,6 @@
 local StatusLine = {
-		StatusLine = {fg = "#16181f", bg = C.gray, },
-		StatusLineNC = {fg = "#16181f", bg = C.gray, },
+		StatusLine = {fg = "#13151b", bg = C.gray, },
+		StatusLineNC = {fg = "#13151b", bg = C.gray, },
 		StatusLineSeparator = {fg = C.bg, },
 		StatusLineTerm = {fg = C.alt_bg, },
 		StatusLineTermNC = {fg = C.alt_bg, },
