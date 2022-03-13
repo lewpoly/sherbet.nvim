941d223c5610325961aff22df124f1bd850d907b
lewpoly <ldixon.design@gmail.com>
2022-03-13 12:41:00 -0400

    trying out green comment again

--- lua/gruvplus/Treesitter.lua
+++ lua/gruvplus/Treesitter.lua
@@ -1,5 +1,5 @@
 local Treesitter = {
-		TSComment = {fg = C.gray, style = "italic", },
+		TSComment = {fg = C.green, style = "italic", },
 		TSAnnotation = {fg = C.yellow, },
 		TSAttribute = {fg = C.cyan, },
 		TSConstructor = {fg = C.cyan, },
