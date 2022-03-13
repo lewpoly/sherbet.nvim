547e06a77044ef7bcd51b8c51c8aa183105f79f4
lewpoly <ldixon.design@gmail.com>
2022-03-13 17:47:44 -0400

    light gray

--- lua/gruvplus/Treesitter.lua
+++ lua/gruvplus/Treesitter.lua
@@ -1,5 +1,5 @@
 local Treesitter = {
-		TSComment = {fg = C.gray, style = "italic", },
+		TSComment = {fg = C.light_gray, style = "italic", },
 		TSAnnotation = {fg = C.yellow, },
 		TSAttribute = {fg = C.cyan, },
 		TSConstructor = {fg = C.cyan, },
