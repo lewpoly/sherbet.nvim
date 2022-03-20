4db365068fe51b728f559b7c9df18c71a6ee0385
lewpoly <ldixon.design@gmail.com>
2022-03-20 12:28:09 -0400

    all comments to gray. blue too similar to others

--- lua/gruvplus/Treesitter.lua
+++ lua/gruvplus/Treesitter.lua
@@ -1,5 +1,5 @@
 local Treesitter = {
-	TSComment = { fg = C.folder_blue },
+	TSComment = { fg = C.gray },
 	TSAnnotation = { fg = C.yellow },
 	TSAttribute = { fg = C.cyan },
 	TSConstructor = { fg = C.cyan },
