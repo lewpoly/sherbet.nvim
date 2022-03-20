0522dfe4b8b108c49c73f4ccb798cb949389ce05
lewpoly <ldixon.design@gmail.com>
2022-03-20 12:23:23 -0400

    try out folder blue for TS

--- lua/gruvplus/Treesitter.lua
+++ lua/gruvplus/Treesitter.lua
@@ -1,5 +1,5 @@
 local Treesitter = {
-	TSComment = { fg = C.light_gray },
+	TSComment = { fg = C.folder_blue },
 	TSAnnotation = { fg = C.yellow },
 	TSAttribute = { fg = C.cyan },
 	TSConstructor = { fg = C.cyan },
