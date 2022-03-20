fef8fb482e95f1c1ef27339aa141ce9efc4dda3a
lewpoly <ldixon.design@gmail.com>
2022-03-20 19:40:41 -0400

    fixed

--- lua/lualine/themes/gruvplus.lua
+++ lua/lualine/themes/gruvplus.lua
@@ -24,10 +24,10 @@ return {
     b = {fg = colors.fg, bg = colors.bg},
     c = {fg = colors.fg, bg = colors.bg}
   },
-  insert = {a = {fg = colors.bg, bg = colors.orange, gui = 'bold'}},
-  visual = {a = {fg = colors.bg, bg = colors.blue, gui = 'bold'}},
-  command = {a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'}},
-  replace = {a = {fg = colors.bg, bg = colors.red1, gui = 'bold'}},
+  insert = {a = {fg = colors.orange, bg = colors.orange, gui = 'bold'}},
+  visual = {a = {fg = colors.blue, bg = colors.blue, gui = 'bold'}},
+  command = {a = {fg = colors.yellow, bg = colors.yellow, gui = 'bold'}},
+  replace = {a = {fg = colors.red1, bg = colors.red1, gui = 'bold'}},
   inactive = {
     a = {fg = colors.gray3, bg = colors.bg},
     b = {fg = colors.gray3, bg = colors.bg},
