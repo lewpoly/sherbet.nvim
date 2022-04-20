0ab3f9b1768efafd512f39d15bb10704ec97e22c
Lew Dixon <ldixon.design@gmail.com>
2022-04-20 09:49:54 -0400

    compare

--- lua/sherbet/palette.lua
+++ lua/sherbet/palette.lua
@@ -1,7 +1,7 @@
 local colors = {
 	fg = "#D4D4D4",
-	-- bg = "#25282f",
-	bg = "#232731",
+	bg = "#25282f",
+	-- bg = "#232731",
   menu_gray = "#1c1e23",
 	line_bg = "#2c323d",
 	alt_bg = "#0d1117",
