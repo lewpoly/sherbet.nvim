bc078eefb8c9b9cdd00477050824ba45d334e0b7
Lew Dixon <ldixon.design@gmail.com>
2022-04-21 17:00:40 -0400

    title to light blue

--- lua/sherbet/Telescope.lua
+++ lua/sherbet/Telescope.lua
@@ -5,8 +5,8 @@ local Telescope = {
 	TelescopeMatching = { fg = C.info_yellow, style = "bold" },
 	TelescopeBorder = { fg = C.fg, bg = Config.transparent_background and "NONE" or C.bg },
 	TelescopePromptPrefix = { fg = C.fg },
-	TelescopeSelectionCaret = { fg = C.vivid_blue },
-	TelescopeTitle = { fg = C.vivid_blue },
+	TelescopeSelectionCaret = { fg = C.fg },
+	TelescopeTitle = { fg = C.light_blue },
 }
 
 return Telescope
