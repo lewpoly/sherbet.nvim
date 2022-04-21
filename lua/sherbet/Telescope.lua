685dcd3b6fe7205c30a3f7a56c35b64176a42292
Lew Dixon <ldixon.design@gmail.com>
2022-04-21 16:50:23 -0400

    reverse title and border

--- lua/sherbet/Telescope.lua
+++ lua/sherbet/Telescope.lua
@@ -3,10 +3,10 @@ local Telescope = {
 	TelescopePromptCounter = { fg = C.accent },
 	TelescopeSelection = { fg = C.fg, bg = C.line_bg },
 	TelescopeMatching = { fg = C.info_yellow, style = "bold" },
-	TelescopeBorder = { fg = C.sign_change, bg = Config.transparent_background and "NONE" or C.bg },
-	TelescopePromptPrefix = { fg = C.fg },
-	TelescopeSelectionCaret = { fg = C.yellow_orange },
-	TelescopeTitle = { fg = C.fg },
+	TelescopeBorder = { fg = C.fg, bg = Config.transparent_background and "NONE" or C.bg },
+	TelescopePromptPrefix = { fg = C.sign_change },
+	TelescopeSelectionCaret = { fg = C.sign_change },
+	TelescopeTitle = { fg = C.sign_change },
 }
 
 return Telescope
