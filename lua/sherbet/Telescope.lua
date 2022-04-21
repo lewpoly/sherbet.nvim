aafe41b9467d626ebd8c6e63375dad4a546b0190
Lew Dixon <ldixon.design@gmail.com>
2022-04-21 16:34:11 -0400

    prompt and title

--- lua/sherbet/Telescope.lua
+++ lua/sherbet/Telescope.lua
@@ -4,9 +4,9 @@ local Telescope = {
 	TelescopeSelection = { fg = C.fg, bg = C.line_bg },
 	TelescopeMatching = { fg = C.info_yellow, style = "bold" },
 	TelescopeBorder = { fg = C.sign_change, bg = Config.transparent_background and "NONE" or C.bg },
-	TelescopePromptPrefix = { fg = C.yellow_orange },
+	TelescopePromptPrefix = { fg = C.fg },
 	TelescopeSelectionCaret = { fg = C.yellow_orange },
-	TelescopeTitle = { fg = C.yellow_orange },
+	TelescopeTitle = { fg = C.fg },
 }
 
 return Telescope
