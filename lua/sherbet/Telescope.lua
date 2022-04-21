a9b2c2a8cbb2d1a29fe28570a202b9f4ecc5e545
Lew Dixon <ldixon.design@gmail.com>
2022-04-21 15:36:10 -0400

    telescope color changes

--- lua/sherbet/Telescope.lua
+++ lua/sherbet/Telescope.lua
@@ -1,7 +1,9 @@
 local Telescope = {
-		TelescopeSelection = {fg = C.hint_blue, },
-		TelescopeMatching = {fg = C.info_yellow, style = "bold", },
-		TelescopeBorder = {fg = C.yellow, bg = Config.transparent_background and "NONE" or C.bg, },
+	TelescopeNormal = { fg = C.gray },
+	TelescopePromptCounter = { fg = C.accent },
+	TelescopeSelection = { fg = C.hint_blue },
+	TelescopeMatching = { fg = C.info_yellow, style = "bold" },
+	TelescopeBorder = { fg = C.alt_bg, bg = Config.transparent_background and "NONE" or C.bg },
 }
 
 return Telescope
