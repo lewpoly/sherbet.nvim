6e93febd130cb64a424faaebc32b58d55655c773
Lew Dixon <ldixon.design@gmail.com>
2022-04-21 17:19:44 -0400

    normal prompt test, removed matching bold

--- lua/sherbet/Telescope.lua
+++ lua/sherbet/Telescope.lua
@@ -1,11 +1,12 @@
 local Telescope = {
 	TelescopeNormal = { fg = C.gray },
+	TelescopePromptNormal = { fg = C.accent },
 	TelescopePromptCounter = { fg = C.accent },
 	TelescopeSelection = { fg = C.fg, bg = C.line_bg },
-	TelescopeMatching = { fg = C.info_yellow, style = "bold" },
+	TelescopeMatching = { fg = C.info_yellow },
 	TelescopeBorder = { fg = C.fg, bg = Config.transparent_background and "NONE" or C.bg },
 	TelescopePromptPrefix = { fg = C.accent },
-	TelescopeSelectionCaret = { fg = C.fg, bg = C.line_bg },
+	TelescopeSelectionCaret = { fg = C.accent, bg = C.line_bg },
 	TelescopeTitle = { fg = C.light_blue },
 }
 
