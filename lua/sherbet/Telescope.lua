b26362c90fb767f775cb4e431d7e10ee89abdb74
Lew Dixon <ldixon.design@gmail.com>
2022-04-21 17:38:07 -0400

    changed matching yellow. enough ricing!

--- lua/sherbet/Telescope.lua
+++ lua/sherbet/Telescope.lua
@@ -3,7 +3,7 @@ local Telescope = {
 	TelescopePromptNormal = { fg = C.fg },
 	TelescopePromptCounter = { fg = C.accent },
 	TelescopeSelection = { fg = C.fg, bg = C.line_bg },
-	TelescopeMatching = { fg = C.info_yellow },
+	TelescopeMatching = { fg = C.yellow_orange },
 	TelescopeBorder = { fg = C.light_blue, bg = Config.transparent_background and "NONE" or C.bg },
 	TelescopePromptPrefix = { fg = C.accent },
 	TelescopeSelectionCaret = { fg = C.accent, bg = C.line_bg },
