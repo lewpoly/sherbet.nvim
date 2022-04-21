f4986276d57c77cb276c4f82e588e9cbba80f11b
Lew Dixon <ldixon.design@gmail.com>
2022-04-21 16:57:54 -0400

    more of the same

--- lua/sherbet/Telescope.lua
+++ lua/sherbet/Telescope.lua
@@ -1,12 +1,12 @@
 local Telescope = {
-	TelescopeNormal = { fg = C.accent },
+	TelescopeNormal = { fg = C.gray },
 	TelescopePromptCounter = { fg = C.accent },
 	TelescopeSelection = { fg = C.fg, bg = C.line_bg },
 	TelescopeMatching = { fg = C.info_yellow, style = "bold" },
 	TelescopeBorder = { fg = C.fg, bg = Config.transparent_background and "NONE" or C.bg },
-	TelescopePromptPrefix = { fg = C.sign_change },
-	TelescopeSelectionCaret = { fg = C.sign_change },
-	TelescopeTitle = { fg = C.sign_change },
+	TelescopePromptPrefix = { fg = C.fg },
+	TelescopeSelectionCaret = { fg = C.vivid_blue },
+	TelescopeTitle = { fg = C.vivid_blue },
 }
 
 return Telescope
