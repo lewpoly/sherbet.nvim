b6e10637048d4e0ccf2d375540cbff6dbb2b7aa7
lewpoly <ldixon.design@gmail.com>
2022-05-28 20:05:04 -0400

    cmp keywords

--- lua/lualine/themes/sherbet.lua
+++ lua/lualine/themes/sherbet.lua
@@ -3,34 +3,34 @@
 -- Credit: Zoltan Dalmadi(lightline)
 -- LuaFormatter off
 local colors = {
-  blue   = '#569cd6',
-  green  = '#6a9955',
-  purple = '#c586c0',
-  red1   = '#d16969',
-  yellow = '#ebcb8b',
-  orange = '#ce9178',
-  fg     = '#d4d4d4',
-  bg     = '#13151b',
-  -- bg     = '#68217a',
-  -- bg     = '#252525',
-  gray1  = '#282c34',
-  gray2  = '#2c323d',
-  gray3  = '#636465',
+	blue = "#569cd6",
+	green = "#6a9955",
+	purple = "#c586c0",
+	red1 = "#d16969",
+	yellow = "#ebcb8b",
+	orange = "#ce9178",
+	fg = "#d4d4d4",
+	bg = "#13151b",
+	-- bg     = '#68217a',
+	-- bg     = '#252525',
+	gray1 = "#282c34",
+	gray2 = "#2c323d",
+	gray3 = "#636465",
 }
 -- LuaFormatter on
 return {
-  normal = {
-    a = {fg = colors.fg, bg = colors.bg, gui = 'bold'},
-    b = {fg = colors.fg, bg = colors.bg},
-    c = {fg = colors.fg, bg = colors.bg}
-  },
-  insert = {a = {fg = colors.orange, bg = colors.bg, gui = 'bold'}},
-  visual = {a = {fg = colors.blue, bg = colors.bg, gui = 'bold'}},
-  command = {a = {fg = colors.yellow, bg = colors.bg, gui = 'bold'}},
-  replace = {a = {fg = colors.red1, bg = colors.bg, gui = 'bold'}},
-  inactive = {
-    a = {fg = colors.gray3, bg = colors.bg},
-    b = {fg = colors.gray3, bg = colors.bg},
-    c = {fg = colors.gray3, bg = colors.bg}
-  }
+	normal = {
+		a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
+		b = { fg = colors.fg, bg = colors.bg },
+		c = { fg = colors.fg, bg = colors.bg },
+	},
+	insert = { a = { fg = colors.orange, bg = colors.bg, gui = "bold" } },
+	visual = { a = { fg = colors.blue, bg = colors.bg, gui = "bold" } },
+	command = { a = { fg = colors.yellow, bg = colors.bg, gui = "bold" } },
+	replace = { a = { fg = colors.red1, bg = colors.bg, gui = "bold" } },
+	inactive = {
+		a = { fg = colors.gray3, bg = colors.bg },
+		b = { fg = colors.gray3, bg = colors.bg },
+		c = { fg = colors.gray3, bg = colors.bg },
+	},
 }
--- lua/sherbet/Cmp.lua
+++ lua/sherbet/Cmp.lua
@@ -15,7 +15,7 @@ local Cmp = {
 		CmpItemKindProperty = {fg = C.ui4_blue, },
 		CmpItemKindEnumMember = {fg = C.light_blue, },
 		CmpItemKindConstant = {fg = C.ui4_blue, },
-		CmpItemKindKeyword = {fg = C.yellow, },
+		CmpItemKindKeyword = {fg = C.yellow_orange, },
 		CmpItemKindModule = {fg = C.purple, },
 		CmpItemKindValue = {fg = C.fg, },
 		CmpItemKindUnit = {fg = C.fg, },
