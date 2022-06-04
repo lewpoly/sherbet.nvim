f80709a1e6d3e48c7044fff8d6f05a7e3f49d026
lewpoly <ldixon.design@gmail.com>
2022-06-04 13:38:00 -0400

    winbar hl

--- lua/sherbet/highlights.lua
+++ lua/sherbet/highlights.lua
@@ -12,6 +12,7 @@ local highlights = {
 	Pmenu = { fg = C.light_gray, bg = C.menu_gray },
 	PmenuSel = { bg = C.ui2_blue },
 	WildMenu = { fg = C.fg, bg = C.ui2_blue },
+	WinBar = { fg = C.fg, bg = C.bg },
 	CursorLineNr = { fg = C.light_gray, style = "bold" },
 	Comment = { fg = C.gray, style = "italic" },
 	Folded = { fg = C.folder_blue, bg = C.bg },
