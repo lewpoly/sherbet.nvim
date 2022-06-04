98db6839a64f2a4c7f13a797be3e21f53c873297
lewpoly <ldixon.design@gmail.com>
2022-06-04 13:32:05 -0400

    buffer visible sign, testing winbar hl

--- lua/sherbet/Buffer.lua
+++ lua/sherbet/Buffer.lua
@@ -7,7 +7,7 @@ local Buffer = {
 		BufferVisible = {fg = C.fg, bg = C.bg, },
 		BufferVisibleIndex = {fg = C.fg, bg = C.bg, },
 		BufferVisibleMod = {fg = C.info_yellow, bg = C.bg, },
-		BufferVisibleSign = {fg = C.gray, bg = C.bg, },
+		BufferVisibleSign = {fg = C.fg, bg = C.bg, },
 		BufferVisibleTarget = {fg = C.red, bg = C.bg, style = "bold", },
 		BufferInactive = {fg = C.gray, bg = C.alt_bg, },
 		BufferInactiveIndex = {fg = C.gray, bg = C.alt_bg, },
