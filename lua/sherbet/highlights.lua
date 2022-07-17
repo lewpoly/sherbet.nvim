443efa2bb1de7fa37f015f8e7d6d641d917c1155
lewpoly <ldixon.design@gmail.com>
2022-07-17 10:11:11 -0400

    titles to aurora cyan

--- lua/sherbet/highlights.lua
+++ lua/sherbet/highlights.lua
@@ -61,7 +61,7 @@ local highlights = {
 	Number = { fg = C.green },
 	Boolean = { fg = C.green },
 	Float = { fg = C.cyan },
-	FloatTitle = { fg = C.gruv_fg },
+	FloatTitle = { fg = C.aurora_cyan },
 	Identifier = { fg = C.search_blue },
 	Function = { fg = C.yellow_orange },
 	Operator = { fg = C.comp_blue },
