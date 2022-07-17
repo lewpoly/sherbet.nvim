f2bb088c68af5a4942e7076c8cf4c84628c8b2ea
lewpoly <ldixon.design@gmail.com>
2022-07-17 10:08:57 -0400

    float title

--- lua/sherbet/highlights.lua
+++ lua/sherbet/highlights.lua
@@ -61,7 +61,7 @@ local highlights = {
 	Number = { fg = C.green },
 	Boolean = { fg = C.green },
 	Float = { fg = C.cyan },
-	FloatTitle = { fg = C.ui_orange },
+	FloatTitle = { fg = C.gruv_fg },
 	Identifier = { fg = C.search_blue },
 	Function = { fg = C.yellow_orange },
 	Operator = { fg = C.comp_blue },
