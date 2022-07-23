53f6eded75ab02fac5adda0840e5f03f0401ebbc
lewpoly <ldixon.design@gmail.com>
2022-07-23 09:01:51 -0400

    nada

--- lua/sherbet/highlights.lua
+++ lua/sherbet/highlights.lua
@@ -17,7 +17,7 @@ local highlights = {
 	Comment = { fg = C.gray, style = "italic" },
 	Folded = { fg = C.folder_blue, bg = C.bg },
 	FoldColumn = { fg = C.folder_blue, bg = C.bg },
-	NormalFloat = { bg = C.menu_gray },
+	NormalFloat = { bg = C.bg },
 	FloatBorder = { fg = C.gray, bg = C.bg },
 	Whitespace = { fg = C.bg },
 	VertSplit = { fg = C.gray, bg = C.bg },
