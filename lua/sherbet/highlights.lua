aceca1a62357ff53a3d5c5253d5d904a025f6fe8
lewpoly <ldixon.design@gmail.com>
2022-07-23 09:40:32 -0400

    normal float

--- lua/sherbet/highlights.lua
+++ lua/sherbet/highlights.lua
@@ -17,7 +17,7 @@ local highlights = {
 	Comment = { fg = C.gray, style = "italic" },
 	Folded = { fg = C.folder_blue, bg = C.bg },
 	FoldColumn = { fg = C.folder_blue, bg = C.bg },
-	NormalFloat = { bg = C.bg },
+	NormalFloat = { bg = C.menu_gray },
 	FloatBorder = { fg = C.gray, bg = C.bg },
 	Whitespace = { fg = C.bg },
 	VertSplit = { fg = C.gray, bg = C.bg },
